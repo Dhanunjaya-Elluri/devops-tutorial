8f560068bf546226cb399aa845b5404e45a98ace
Dhanunjaya Elluri <dhanunjayet@gmail.com>
2024-04-11 20:43:10 +0200

    Modified About Route

--- flask-app/app.py
+++ flask-app/app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, Flask!'
 
-@app.route('/about')
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+
+@app.route("/about")
 def about():
-    return 'About page'
+    return "About page Dhanunjay"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
